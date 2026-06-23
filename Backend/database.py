2200eb02a49f8370a0dc302461409c0e085371d5
Eklavya <eklavyaanand13@gmail.com>
2026-06-23 13:53:28 +0530

    Use environment database url

--- Backend/database.py
+++ Backend/database.py
@@ -3,8 +3,9 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:JohnCena!2356@localhost/trade_journal"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
