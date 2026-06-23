0ab64637a7ef1d68954662406f148a8023da2c58
Eklavya <eklavyaanand13@gmail.com>
2026-06-23 17:57:57 +0530

    Fix HTTPS API URLs

--- Backend/database.py
+++ Backend/database.py
@@ -7,7 +7,8 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+    pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit = False,
