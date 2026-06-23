415228e00eae931f3628f88209e6861e79180a3d
Eklavya Anand Singh <eklavyaanand13@gmail.com>
2026-06-23 17:09:45 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
-import "./styles/index.css";
+import "./Styles/index.css";
 import "./Styles/landingPage.css";
 import "./Styles/dashboard.css";
 import "./Styles/tradeform.css";
