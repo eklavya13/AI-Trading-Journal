a701b2a5094e6cf13ecc252016b204f837167582
Eklavya Anand Singh <eklavyaanand13@gmail.com>
2026-06-23 16:50:17 +0530

    Update SetupChart.jsx

--- Frontend/src/components/SetupChart.jsx
+++ Frontend/src/components/SetupChart.jsx
@@ -4,7 +4,7 @@ import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 function SetupChart (){
   const [setupData, setSetupData] = useState([]);
   useEffect(() => {
-  fetch("http://192.168.1.7:8000/setup-performance")
+  fetch("http://ai-trading-journal-backend.onrender.com/setup-performance")
     .then((res) => res.json())
     .then((data) => {
       setSetupData(data);
