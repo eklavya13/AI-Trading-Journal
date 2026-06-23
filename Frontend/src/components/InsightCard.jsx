c7874ea983cd7a24df06054d33abcca74b52b584
Eklavya Anand Singh <eklavyaanand13@gmail.com>
2026-06-23 16:49:30 +0530

    Update API endpoints in InsightCard component

--- Frontend/src/components/InsightCard.jsx
+++ Frontend/src/components/InsightCard.jsx
@@ -5,13 +5,13 @@ function Insight() {
   const [bestSession, setBestSession] = useState(null);
 
   useEffect(() => {
-    fetch("http://192.168.1.7:8000/best_setup")
+    fetch("http://ai-trading-journal-backend.onrender.com/best_setup")
       .then((response) => response.json())
       .then((data) => {
         setBestSetup(data);
       });
 
-    fetch("http://192.168.1.7:8000/best-session")
+    fetch("http://ai-trading-journal-backend.onrender.com/best-session")
       .then((response) => response.json())
       .then((data) => {
         setBestSession(data);
